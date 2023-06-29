f0c4b4e6595ca961cfca4814d6f972d995b9e536
Marcos Fiuza <marcosfiuza@gmail.com>
2023-06-29 09:23:58 -0300

    fix: rename store catalog module facade interface

--- src/modules/store-catalog/facade/store-catalog.facade.interface.ts
+++ src/modules/store-catalog/facade/store-catalog.facade.interface.ts
@@ -1,7 +1,7 @@
 import { FindAllProductsFacadeOutputDto } from "./find-all-products.facade.dto";
 import { FindProductFacadeInputDto, FindProductFacadeOutputDto } from "./find-product.facade.dto";
 
-export default interface ProductAdmFacadeInterface {
+export default interface StoreCatalogFacadeInterface {
     findAllProducts(): Promise<FindAllProductsFacadeOutputDto>;
     findProduct(input: FindProductFacadeInputDto): Promise<FindProductFacadeOutputDto>;
 }
